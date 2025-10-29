0f0bfbd9b850e45337bfed548e9f539479abdfe5
Fabian Steeg <steeg@hbz-nrw.de>
2025-10-29 15:40:50 +0100

    Switch from annotation-based to functional/HTTP WebFlux.fn style
    Separate routes and handlers, handle HTTP requests and responses

---
+++ src/main/java/org/lobid/gnd_ui_lobid/controller/RouterConfig.java
@@ -0,0 +1,30 @@
+package org.lobid.gnd_ui_lobid.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.HandlerFilterFunction;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class RouterConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> detailsRoutes(DetailsHandler handler) {
+        return RouterFunctions.route()
+                .GET("/", handler::index)
+                // Define URL route for GND entry with ID, e.g. `/4031483-2`:
+                .GET("/{id}", handler::byId)
+                .filter(addIsDevserver())
+                .build();
+    }
+
+    private HandlerFilterFunction<ServerResponse, ServerResponse> addIsDevserver() {
+        return (request, next) -> next.handle(ServerRequest.from(request)
+                .attribute("isDevserver", "1".equals(request.headers().firstHeader("X-Devserver")))
+                .build());
+    }
+
+}
--- src/test/java/org/lobid/gnd_ui_lobid/DetailsTests.java
+++ src/test/java/org/lobid/gnd_ui_lobid/DetailsTests.java
@@ -1,48 +1,26 @@
 package org.lobid.gnd_ui_lobid;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import java.io.IOException;
 import org.htmlunit.WebClient;
 import org.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.lobid.gnd_ui_lobid.config.HtmlUnitConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @Import(HtmlUnitConfig.class)
 public class DetailsTests {
 
-    // Test via WebFlux WebTestClient:
-
-    @Autowired
-    private ApplicationContext context;
-
-    private WebTestClient webTestClient;
-
-    @BeforeEach
-    void setUp() {
-        webTestClient = WebTestClient.bindToApplicationContext(context).build();
-    }
-
-    @Test
-    public void testDetailsRoute() {
-        webTestClient.get().uri("/4031483-2")
-                .accept(MediaType.TEXT_HTML)
-                .exchange()
-                .expectStatus()
-                .is2xxSuccessful();
-    }
-
-    // Test via HtmlUnit WebClient:
+    // Test rendered templates, using HtmlUnit:
+    // https://www.htmlunit.org/gettingStarted.html
 
     @Autowired
     private WebClient webClient;
@@ -50,11 +28,22 @@ public class DetailsTests {
     @LocalServerPort
     private int port;
 
+    HtmlPage detailsPage;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        detailsPage = webClient.getPage("http://localhost:" + port + "/4031483-2");
+    }
+
+    @Test
+    public void testDetailsRoute() throws IOException {
+        assertThat(detailsPage.getWebResponse().isSuccess());
+        assertThat(detailsPage.getContentType())
+                .isEqualTo(MediaType.TEXT_HTML_VALUE);
+    }
+
     @Test
     public void testDetailsView() throws Exception {
-        // Test rendered templates, using HtmlUnit:
-        // https://www.htmlunit.org/gettingStarted.html
-        HtmlPage detailsPage = webClient.getPage("http://localhost:" + port + "/4031483-2");
         assertThat(detailsPage.getTitleText())
                 .isEqualTo("4031483-2 - gnd-ui-lobid");
         assertThat(detailsPage.asNormalizedText())
