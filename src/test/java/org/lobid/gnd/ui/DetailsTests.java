533b963c6d9c45f4d6559b2ec52bff9e28d4d7a5
Fabian Steeg <steeg@hbz-nrw.de>
2025-11-21 14:10:50 +0100

    Set up tests to run against prod and test systems, fix title (#1)

--- src/test/java/org/lobid/gnd/ui/DetailsTests.java
+++ src/test/java/org/lobid/gnd/ui/DetailsTests.java
@@ -5,8 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import org.htmlunit.WebClient;
 import org.htmlunit.html.HtmlPage;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.lobid.gnd.ui.config.HtmlUnitConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -23,26 +23,29 @@ public class DetailsTests {
     // Test rendered templates, using HtmlUnit:
     // https://www.htmlunit.org/gettingStarted.html
 
+    private static final String PRODUCTION = "https://lobid.org/gnd";
+    private static final String DEVELOPMENT = "http://localhost";
+    private static final String COLOGNE = "4031483-2";
+
     @Autowired private WebClient webClient;
 
     @LocalServerPort private int port;
 
     HtmlPage detailsPage;
 
-    @BeforeEach
-    public void setUp() throws IOException {
-        detailsPage = webClient.getPage("http://localhost:" + port + "/4031483-2");
-    }
-
-    @Test
-    public void testDetailsRoute() throws IOException {
+    @ParameterizedTest
+    @ValueSource(strings = {PRODUCTION, DEVELOPMENT})
+    public void testDetailsRoute(String baseUrl) throws IOException {
+        detailsPage = pageFor(baseUrl, COLOGNE);
         assertThat(detailsPage.getWebResponse().isSuccess());
         assertThat(detailsPage.getContentType()).isEqualTo(MediaType.TEXT_HTML_VALUE);
     }
 
-    @Test
-    public void testDetailsView() throws Exception {
-        assertThat(detailsPage.getTitleText()).isEqualTo("4031483-2 - lobid-gnd");
+    @ParameterizedTest
+    @ValueSource(strings = {PRODUCTION, DEVELOPMENT})
+    public void testDetailsView(String baseUrl) throws IOException {
+        detailsPage = pageFor(baseUrl, COLOGNE);
+        assertThat(detailsPage.getTitleText()).isEqualTo("Köln");
         assertThat(detailsPage.asNormalizedText())
                 .contains("https://d-nb.info/gnd/4031483-2")
                 .contains("Köln")
@@ -50,5 +53,10 @@ public class DetailsTests {
                 .contains("Kolonie")
                 .contains("Kūlūniyā")
                 .contains("Nordrhein-Westfalen");
+    }
+
+    private HtmlPage pageFor(String baseUrl, String gndId) throws IOException {
+        String baseUrlWithPort = baseUrl + (baseUrl.contains("localhost") ? ":" + port : "");
+        return webClient.getPage(baseUrlWithPort + "/" + gndId);
     }
 }
