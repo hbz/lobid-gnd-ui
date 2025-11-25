c26d4c6f1173ac4dbf410f2634a7f85f98bf384a
Fabian Steeg <steeg@hbz-nrw.de>
2025-11-25 11:40:48 +0100

    Add details view tests for production system (#1)

--- src/test/java/org/lobid/gnd/ui/DetailsTests.java
+++ src/test/java/org/lobid/gnd/ui/DetailsTests.java
@@ -3,7 +3,9 @@ package org.lobid.gnd.ui;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.util.List;
 import org.htmlunit.WebClient;
+import org.htmlunit.html.DomElement;
 import org.htmlunit.html.HtmlPage;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -43,7 +45,7 @@ public class DetailsTests {
 
     @ParameterizedTest
     @ValueSource(strings = {PRODUCTION, DEVELOPMENT})
-    public void testDetailsView(String baseUrl) throws IOException {
+    public void testDetailsViewFields(String baseUrl) throws IOException {
         detailsPage = pageFor(baseUrl, COLOGNE);
         assertThat(detailsPage.getTitleText()).isEqualTo("Köln");
         assertThat(detailsPage.asNormalizedText())
@@ -53,6 +55,58 @@ public class DetailsTests {
                 .contains("Kolonie")
                 .contains("Kūlūniyā")
                 .contains("Nordrhein-Westfalen");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {PRODUCTION /*, DEVELOPMENT*/})
+    public void testDetailsViewHeader(String baseUrl) throws IOException {
+        assertThat(pageFor(baseUrl, COLOGNE).getElementsByTagName("h1").getFirst().getTextContent())
+                .contains("Köln")
+                .contains("Gebietskörperschaft oder Verwaltungseinheit")
+                .contains("Geografikum")
+                .contains("Hauptstadt des Regierungsbezirks Köln");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {PRODUCTION /*, DEVELOPMENT*/})
+    public void testDetailsViewLinks(String baseUrl) throws IOException {
+        assertThat(pageFor(baseUrl, COLOGNE).getElementsByTagName("a").toString())
+                .contains("TerritorialCorporateBodyOrAdministrativeUnit")
+                .contains("PlaceOrGeographicName")
+                .contains("4031483-2.json")
+                .contains("https://www.stadt-koeln.de/")
+                .contains("https://d-nb.info/standards/vocab/gnd/geographic-area-code#XA-DE-NW")
+                .contains("https://www.herder-institut.de/bildkatalog/gnd/4031483-2")
+                .contains("https://www.dnb.de/lds")
+                .contains("https://d-nb.info/gnd/4031483-2/about/lds.rdf")
+                .contains("https://d-nb.info/gnd/4031483-2/about/lds.ttl")
+                .contains("https://www.dnb.de/entityfacts")
+                .contains("http://hub.culturegraph.org/entityfacts/4031483-2")
+                .contains("https://creativecommons.org/publicdomain/zero/1.0/");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {PRODUCTION /*, DEVELOPMENT*/})
+    public void testDetailsViewMap(String baseUrl) throws IOException {
+        List<DomElement> mapElements = pageFor(baseUrl, COLOGNE).getElementsById("authority-map");
+        assertThat(mapElements).isNotEmpty();
+        assertThat(mapElements.getFirst().getElementsByTagName("a").toString())
+                .contains("http://leafletjs.com")
+                .contains("http://osm.org/copyright");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {PRODUCTION /*, DEVELOPMENT*/})
+    public void testDetailsViewImage(String baseUrl) throws IOException {
+        HtmlPage detailsPage = pageFor(baseUrl, COLOGNE);
+        assertThat(detailsPage.getByXPath("//img[@alt='Köln']")).isNotEmpty();
+        assertThat(detailsPage.getBody().asNormalizedText())
+                .contains("Bildquelle")
+                .contains("Wikimedia Commons")
+                .contains("CC");
+        assertThat(detailsPage.getElementsByTagName("a").toString())
+                .contains("https://commons.wikimedia.org")
+                .contains("https://creativecommons.org");
     }
 
     private HtmlPage pageFor(String baseUrl, String gndId) throws IOException {
