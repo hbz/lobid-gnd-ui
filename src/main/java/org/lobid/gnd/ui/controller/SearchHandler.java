7b53fece034e8a3fa8ef388633ee91fc9b293632
Fabian Steeg <steeg@hbz-nrw.de>
2026-03-18 18:34:42 +0100

    Add details in search results, based on `json:suggest` request (#8)

--- src/main/java/org/lobid/gnd/ui/controller/SearchHandler.java
+++ src/main/java/org/lobid/gnd/ui/controller/SearchHandler.java
@@ -13,33 +13,52 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 @Component
 public class SearchHandler {
+
     public Mono<ServerResponse> byQ(ServerRequest request) {
         try {
-            return gndSearch(request.queryParam("q").orElse(""))
+            String q = request.queryParam("q").orElse("");
+            return Mono.zip(
+                            call("https://lobid.org/gnd/search?q=" + q),
+                            call("https://lobid.org/gnd/search?format=json:suggest&q=" + q))
                     .flatMap(toResponse("search", request));
         } catch (Exception e) {
             return errorResponse(request, 500, "Search failed: " + e.getMessage());
         }
     }
 
-    private Mono<Map<String, Object>> gndSearch(String q) {
+    private Mono<Map<String, Object>> call(String uri) {
         return WebClient.create()
                 .get()
-                .uri("https://lobid.org/gnd/search?q={q}", q)
+                .uri(uri)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(JsonNode.class)
-                .map(json -> new ObjectMapper().convertValue(json, new TypeReference<>() {}));
+                .map(toJavaMap());
     }
 
-    private Function<Map<String, Object>, Mono<ServerResponse>> toResponse(
-            String template, ServerRequest request) {
-        return search -> {
-            return ServerResponse.ok()
-                    .render(template, Map.of("search", search, "request", request.attributes()));
+    private Function<JsonNode, Map<String, Object>> toJavaMap() {
+        return json ->
+                json.isArray()
+                        ? Map.of("array", new ObjectMapper().convertValue(json, Map[].class))
+                        : new ObjectMapper().convertValue(json, new TypeReference<>() {});
+    }
+
+    private Function<Tuple2<Map<String, Object>, Map<String, Object>>, Mono<ServerResponse>>
+            toResponse(String template, ServerRequest request) {
+        return results -> {
+            Map<String, Map<String, Object>> model =
+                    Map.of(
+                            "search",
+                            results.getT1(),
+                            "suggest",
+                            results.getT2(),
+                            "request",
+                            request.attributes());
+            return ServerResponse.ok().render(template, model);
         };
     }
 }
