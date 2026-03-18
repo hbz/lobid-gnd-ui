c169b41af007c62470fe415e65c5da5c4d61eeb1
Fabian Steeg <steeg@hbz-nrw.de>
2026-03-18 12:54:30 +0100

    Set up basic search template (#8)

--- src/main/java/org/lobid/gnd/ui/controller/SearchHandler.java
+++ src/main/java/org/lobid/gnd/ui/controller/SearchHandler.java
@@ -2,7 +2,14 @@ package org.lobid.gnd.ui.controller;
 
 import static org.lobid.gnd.ui.controller.ErrorHandler.errorResponse;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import java.util.function.Function;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
@@ -11,10 +18,28 @@ import reactor.core.publisher.Mono;
 public class SearchHandler {
     public Mono<ServerResponse> byQ(ServerRequest request) {
         try {
-            String q = request.queryParam("q").orElse("");
-            return errorResponse(request, 501, "Not Implemented, q=" + q);
+            return gndSearch(request.queryParam("q").orElse(""))
+                    .flatMap(toResponse("search", request));
         } catch (Exception e) {
             return errorResponse(request, 500, "Search failed: " + e.getMessage());
         }
+    }
+
+    private Mono<Map<String, Object>> gndSearch(String q) {
+        return WebClient.create()
+                .get()
+                .uri("https://lobid.org/gnd/search?q={q}", q)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .map(json -> new ObjectMapper().convertValue(json, new TypeReference<>() {}));
+    }
+
+    private Function<Map<String, Object>, Mono<ServerResponse>> toResponse(
+            String template, ServerRequest request) {
+        return search -> {
+            return ServerResponse.ok()
+                    .render(template, Map.of("search", search, "request", request.attributes()));
+        };
     }
 }
