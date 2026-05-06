975ab8fd7a4650b4a7e70444c6fab7b51f3791b2
Fabian Steeg <steeg@hbz-nrw.de>
2026-05-06 13:37:05 +0200

    Extract label and image enrichment to service, add type labels (#7)

--- src/main/java/org/lobid/gnd/ui/LobidGndApiService.java
+++ src/main/java/org/lobid/gnd/ui/LobidGndApiService.java
@@ -4,12 +4,19 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -34,7 +41,7 @@ public class LobidGndApiService {
     public Mono<Map<String, Object>> search(MultiValueMap<String, String> params) {
         Function<UriBuilder, URI> uriFunction =
                 builder -> builder.path("/search").queryParams(params).build();
-        return gndCall(uriFunction).map(json -> javaMap(json));
+        return gndCall(uriFunction).map(this::javaMap);
     }
 
     public Mono<Map<String, Object>> suggest(MultiValueMap<String, String> params) {
@@ -43,7 +50,9 @@ public class LobidGndApiService {
 
     public Mono<Map<String, Object>> entity(String gndId) {
         Function<UriBuilder, URI> uriFunction = builder -> builder.path("/{gndId}").build(gndId);
-        return gndCall(uriFunction).map(json -> javaMap(json));
+        return gndCall(uriFunction)
+                .flatMap(this::withPropertyAndTypeLabels)
+                .map(this::withImageUrlAndAttribution);
     }
 
     public Mono<Map<String, Object>> randomEntity() {
@@ -54,10 +63,12 @@ public class LobidGndApiService {
                                 .queryParam("size", "1")
                                 .queryParam("from", String.valueOf(new Random().nextInt(25000)))
                                 .build();
-        return gndCall(uriFunction).map(json -> firstMemberAsMap(json));
+        return gndCall(uriFunction)
+                .map(this::firstMemberAsMap)
+                .map(this::withImageUrlAndAttribution);
     }
 
-    public Mono<String> label(String kind, String id) {
+    private Mono<String> label(String kind, String id) {
         Function<UriBuilder, URI> uriFunction =
                 builder ->
                         builder.path("/reconcile/suggest/{kind}")
@@ -125,5 +136,75 @@ public class LobidGndApiService {
                 .map(result -> result.get("name").asText())
                 .defaultIfEmpty("No " + kind + " label for " + id)
                 .next();
+    }
+
+    private Mono<Map<String, Object>> withPropertyAndTypeLabels(JsonNode gndEntity) {
+        Stream<String> properties = toStream(gndEntity.fieldNames());
+        Stream<String> types = toStream(gndEntity.get("type").elements()).map(JsonNode::asText);
+        return Flux.concat(labelsFor(properties, "property"), labelsFor(types, "type"))
+                .collect(
+                        () -> new HashMap<String, String>(),
+                        (map, entry) -> map.put(entry.getKey(), entry.getValue()))
+                .map(
+                        labels -> {
+                            Map<String, Object> model = javaMap(gndEntity);
+                            model.put("labels", labels);
+                            return model;
+                        });
+    }
+
+    private <T> Stream<T> toStream(Iterator<T> fieldNames) {
+        Iterable<T> iterable = () -> fieldNames;
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+
+    private Flux<Entry<String, String>> labelsFor(Stream<String> keys, String kind) {
+        return Flux.fromStream(keys)
+                .flatMap(key -> label(kind, key).map(label -> Map.entry(key, label)));
+    }
+
+    private Map<String, Object> withImageUrlAndAttribution(Map<String, Object> javaMap) {
+        if (javaMap.containsKey("depiction")) {
+            @SuppressWarnings("unchecked")
+            var depictions = (List<Map<String, Object>>) javaMap.get("depiction");
+            String imageAttribution = createAttribution(depictions.getFirst());
+            String proxyPrefix = "https://lobid.org/imagesproxy?url=";
+            javaMap.put("imageUrl", proxyPrefix + depictions.getFirst().get("thumbnail"));
+            javaMap.put("imageAttribution", String.format("Bildquelle: %s", imageAttribution));
+        }
+        return javaMap;
+    }
+
+    private String createAttribution(Map<String, Object> depiction) {
+        @SuppressWarnings("unchecked")
+        Map<String, Object> license =
+                Optional.ofNullable(((List<Map<String, Object>>) depiction.get("license")))
+                        .map(list -> list.get(0))
+                        .orElse(Collections.emptyMap());
+        String artist = findText(depiction, "creatorName").replaceAll("(Unknown.*){2}", "$1");
+        String licenseText = findText(license, "abbr");
+        String licenseUrl = findText(license, "id");
+        String fileSourceUrl = findText(depiction, "url");
+        String urlForLicense = licenseUrl.isEmpty() ? fileSourceUrl : licenseUrl;
+        return attributionHtml(artist, licenseText, fileSourceUrl, urlForLicense);
+    }
+
+    private String findText(Map<String, Object> map, String field) {
+        Object value = map.get(field);
+        value = value instanceof List ? ((List<?>) value).get(0) : value;
+        return value != null ? value.toString().replace("\n", " ").trim() : "";
+    }
+
+    private String attributionHtml(
+            String artist, String license, String fileSourceUrl, String licenseUrl) {
+        return String.format(
+                "%s%s%s",
+                no(artist).orElse(String.format("%s | ", artist)),
+                String.format("<a href='%s'>Wikimedia Commons</a>", fileSourceUrl),
+                no(license).orElse(String.format(" | <a href='%s'>%s</a>", licenseUrl, license)));
+    }
+
+    private Optional<String> no(String string) {
+        return string.isEmpty() ? Optional.of("") : Optional.empty();
     }
 }
--- src/main/java/org/lobid/gnd/ui/controller/DetailsHandler.java
+++ src/main/java/org/lobid/gnd/ui/controller/DetailsHandler.java
@@ -2,18 +2,15 @@ package org.lobid.gnd.ui.controller;
 
 import static org.lobid.gnd.ui.controller.ErrorHandler.errorResponse;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.lobid.gnd.ui.LobidGndApiService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -36,67 +33,25 @@ public class DetailsHandler {
         }
     }
 
+    @Component("details")
+    public static class DetailsHelper {
+        public String label(Map<String, String> labels, Object value) {
+            return switch (value) {
+                case List<?> list ->
+                        list.stream().map(v -> label(labels, v)).collect(Collectors.joining(", "));
+                case String s -> labels.getOrDefault(s, value.toString());
+                default -> value.toString();
+            };
+        }
+    }
+
     private Function<Map<String, Object>, Mono<ServerResponse>> toResponse(
             String template, ServerRequest request) {
         return gndEntity -> {
-            return Flux.fromIterable(gndEntity.keySet())
-                    .flatMap(key -> gnd.label("property", key).map(label -> Map.entry(key, label)))
-                    .collect(
-                            () -> new HashMap<String, String>(),
-                            (map, entry) -> map.put(entry.getKey(), entry.getValue()))
-                    .flatMap(
-                            labels -> {
-                                Map<String, Object> model = new HashMap<>();
-                                model.put("entity", withImageUrlAndAttribution(gndEntity));
-                                model.put("labels", labels);
-                                model.put("request", request.attributes());
-                                return ServerResponse.ok().render(template, model);
-                            });
+            Map<String, Map<String, Object>> model =
+                    Map.of("entity", gndEntity, "request", request.attributes());
+            // Render Thymeleaf template (in src/main/resources/templates) with model:
+            return ServerResponse.ok().render(template, model);
         };
-    }
-
-    static Map<String, Object> withImageUrlAndAttribution(Map<String, Object> javaMap) {
-        if (javaMap.containsKey("depiction")) {
-            @SuppressWarnings("unchecked")
-            var depictions = (List<Map<String, Object>>) javaMap.get("depiction");
-            String imageAttribution = createAttribution(depictions.getFirst());
-            String proxyPrefix = "https://lobid.org/imagesproxy?url=";
-            javaMap.put("imageUrl", proxyPrefix + depictions.getFirst().get("thumbnail"));
-            javaMap.put("imageAttribution", String.format("Bildquelle: %s", imageAttribution));
-        }
-        return javaMap;
-    }
-
-    private static String createAttribution(Map<String, Object> depiction) {
-        @SuppressWarnings("unchecked")
-        Map<String, Object> license =
-                Optional.ofNullable(((List<Map<String, Object>>) depiction.get("license")))
-                        .map(list -> list.get(0))
-                        .orElse(Collections.emptyMap());
-        String artist = findText(depiction, "creatorName").replaceAll("(Unknown.*){2}", "$1");
-        String licenseText = findText(license, "abbr");
-        String licenseUrl = findText(license, "id");
-        String fileSourceUrl = findText(depiction, "url");
-        String urlForLicense = licenseUrl.isEmpty() ? fileSourceUrl : licenseUrl;
-        return attributionHtml(artist, licenseText, fileSourceUrl, urlForLicense);
-    }
-
-    private static String findText(Map<String, Object> map, String field) {
-        Object value = map.get(field);
-        value = value instanceof List ? ((List<?>) value).get(0) : value;
-        return value != null ? value.toString().replace("\n", " ").trim() : "";
-    }
-
-    private static String attributionHtml(
-            String artist, String license, String fileSourceUrl, String licenseUrl) {
-        return String.format(
-                "%s%s%s",
-                no(artist).orElse(String.format("%s | ", artist)),
-                String.format("<a href='%s'>Wikimedia Commons</a>", fileSourceUrl),
-                no(license).orElse(String.format(" | <a href='%s'>%s</a>", licenseUrl, license)));
-    }
-
-    private static Optional<String> no(String string) {
-        return string.isEmpty() ? Optional.of("") : Optional.empty();
     }
 }
