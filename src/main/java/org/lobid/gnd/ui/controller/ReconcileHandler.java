c98148ffe465793032cda5892f785d46a1ec5af4
Fabian Steeg <steeg@hbz-nrw.de>
2026-09-24 16:18:09 +0200

    Extract `reconcile` URL to config file (#25)

--- src/main/java/org/lobid/gnd/ui/controller/ReconcileHandler.java
+++ src/main/java/org/lobid/gnd/ui/controller/ReconcileHandler.java
@@ -1,6 +1,7 @@
 package org.lobid.gnd.ui.controller;
 
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -9,17 +10,12 @@ import reactor.core.publisher.Mono;
 @Component
 public class ReconcileHandler {
 
+    @Value("${app.reconcile}")
+    private String reconcileUrl;
+
     public Mono<ServerResponse> reconcile(ServerRequest request) {
-        String uri = request.uri().toString();
         Map<String, Object> model =
-                Map.of(
-                        "request",
-                        request.attributes(),
-                        "baseUrl",
-                        (request.queryParams().isEmpty()
-                                        ? uri
-                                        : uri.substring(0, uri.lastIndexOf('?')))
-                                + "/");
+                Map.of("request", request.attributes(), "baseUrl", reconcileUrl);
         return ServerResponse.ok().render("reconcile", model);
     }
 }
